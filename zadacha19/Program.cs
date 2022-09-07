0d94ee40e1a79f8eb7bde19d5fc6991cbde4e2ed
Medet <medetnurm@gmail.com>
2022-09-07 21:57:45 +0600

    zadacha19

---
+++ zadacha19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10) 
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
