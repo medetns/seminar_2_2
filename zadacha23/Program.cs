73693ff2c18b4d43dd15c773f5f4cdf9d0d4bc21
Medet <medetnurm@gmail.com>
2022-09-07 22:16:49 +0600

    zadacha23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход 
+число (N) и выдаёт таблицу кубов чисел от 1 до N. */
+
+int number = int.Parse(Console.ReadLine());
+
+int index = 1;
+
+while (index <= number)
+{
+    Console.WriteLine(index*index*index);
+    index++;
+}
